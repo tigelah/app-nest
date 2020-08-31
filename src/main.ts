cf8becf3e00a29cac1ecb6131a816a6084199916
tigelah <lokoportitimao2009@hotmail.com>
2020-08-31 14:18:27 -0300

    adicionando o dockerfile e github workflows para CI

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import { EntityNotFoundExceptionFilter } from './category/exception-filters/entity-not-found.exception-filter';
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
-  
-  const options = new DocumentBuilder()
-    .setTitle('Nest.js API Fullcycle')
-    .setDescription('Documentação da API do Nest.js')
-    .build();
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { MEDIA_URL } from './file';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  app.setViewEngine('hbs');
+  app.useGlobalPipes(
+    new ValidationPipe({ errorHttpStatusCode: 422, whitelist: true }),
+  );
+  app.useStaticAssets(join(__dirname, '..', 'media'), { prefix: MEDIA_URL });
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
+
+  const options = new DocumentBuilder()
+    .setTitle('CodeSlack')
+    .setVersion('1.0')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
